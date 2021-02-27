06c9b16cfab6bebfcfbe61d54dc19b60e6334aaf
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2021-02-27 17:46:21 +0300

    fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,16 +1,13 @@
 import colorHealth from '../app';
-test('healthy', () => {
-  
-    expect(colorHealth("Маг", 80)).toBe('healthy');
-  });
-  
-test('wounded', () => {
-    
 
-    expect(colorHealth("Маг", 40)).toBe('wounded');
-  });
-  
-  test('critical', () => {
-  
-    expect(colorHealth("Маг", 5)).toBe('critical');
-  });
+test('healthy', () => {
+  expect(colorHealth('Маг', 80)).toBe('healthy');
+});
+
+test('wounded', () => {
+  expect(colorHealth('Маг', 40)).toBe('wounded');
+});
+
+test('critical', () => {
+  expect(colorHealth('Маг', 5)).toBe('critical');
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 export default function colorHealth(name, health) {
-    let status = '';
-    if (health > 50) {
-      status = 'healthy';
-    } else if (health > 14 && health < 51) {
-      status = 'wounded';
-    } else if (health < 15) {
-      status = 'critical';
-    }
-    return status;
+  let status = '';
+  if (health > 50) {
+    status = 'healthy';
+  } else if (health > 14 && health < 51) {
+    status = 'wounded';
+  } else if (health < 15) {
+    status = 'critical';
   }
+  return status;
+}
