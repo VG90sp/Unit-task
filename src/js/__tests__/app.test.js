c5040034514eceef6ee63fde8b4584210ae79188
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2021-02-27 17:58:59 +0300

    fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
 import colorHealth from '../app';
 
 test('healthy', () => {
+  // eslint-disable-next-line no-undef
   expect(colorHealth('Маг', 80)).toBe('healthy');
 });
 
@@ -9,5 +11,6 @@ test('wounded', () => {
 });
 
 test('critical', () => {
+  // eslint-disable-next-line no-undef
   expect(colorHealth('Маг', 5)).toBe('critical');
 });
